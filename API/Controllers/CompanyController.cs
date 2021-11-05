121a3fed53d6f2ad17015bdd9f8b12dc84da786b
Jasurbek Khasanboyev <81855769+jasurbuz@users.noreply.github.com>
2021-11-05 14:27:25 +0500

    added company controller

---
+++ API/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yol.Services.IRepository;
+
+namespace Yol.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        //private readonly IMapper _mapper;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        //public async Task<IActionResult> AddCompany()
+    }
+}
